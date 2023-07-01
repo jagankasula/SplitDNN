292fad7f0afc8926902f5df752058051e91fd221
Jagannath Guptha Kasula <jagan@jagannaths-air.lan>
2023-06-30 20:53:09 -0500

    Synchronus vt

---
+++ vt/vtClientSync.py
@@ -0,0 +1,193 @@
+from vit_keras import vit
+from tensorflow import keras
+from tornado import httpclient
+from tornado.queues import Queue
+from PIL import Image
+from vtutils import Config, Logger, write_to_csv
+
+import cv2
+import datetime
+import pickle
+import tensorflow as tf
+import warnings
+import io
+
+io.StringIO
+warnings.filterwarnings('ignore')
+
+# Read the configurations from the config file.
+config = Config.get_config()
+
+metrics_headers = ['split no.', 'flops', 'total_processing_time', 'single_frame_time', 'left_output_size']
+
+
+# Assign the configurations to the global variables.
+device = config['client_device']
+split_point = config['split_point']
+url = config['url']
+frames_to_process = config['frames_to_process']
+
+# Initialize queue for storing the output of the frames that were processed on the client (left) side.
+q = Queue(maxsize=2)
+
+# Initialize the start time to None. This value will be set in main_runner when it is initialized.
+start_time = None
+
+# Track total responses handled.
+total_handled_responses = 0
+
+left_output_size = 0
+
+with tf.device(device):
+  model = vit.build_model(image_size=224, patch_size=16, classes=1000, num_layers=19,
+                        hidden_size=768, num_heads=12, name= 'vit_custom', mlp_dim=3072,
+                        activation='softmax', include_top=True,
+                        representation_size=None)
+  
+  
+  split_layer = model.layers[split_point]
+
+  print(split_layer.name)
+  
+  left_model = keras.Model(inputs=model.input, outputs=split_layer.output)
+
+#   profile = model_profiler(left_model, frames_to_process)
+
+#   df = pd.read_csv(io.StringIO(profile), sep='|', skiprows=0, skipinitialspace=True)
+
+#   df.columns = df.columns.str.strip()
+
+  flops = 0
+
+        
+def producer_video_left(img, left_model):
+
+    tensor = convert_image_to_tensor(img)
+
+    out_left = left_model(tensor)
+    
+    return out_left
+
+def convert_image_to_tensor(img):
+
+    img_rgb = Image.fromarray(img).convert('RGB')
+    tensor = tf.image.resize(img_rgb, [224, 224]) 
+    tensor  = tf.expand_dims(tensor, axis=0)
+
+    # strategy = tf.distribute.experimental.CentralStorageStrategy()
+    # with strategy.scope():
+    #     gpu_tensor = tf.constant(tensor)
+
+    return tensor
+
+def get_left_model(split_point):
+    
+    split_layer = model.layers[split_point]
+
+    print(split_layer.name)
+  
+    left_model = keras.Model(inputs=model.input, outputs=split_layer.output)
+
+    return left_model
+
+# Returns JSON body for sending to server.
+def get_request_body(left_output, frame_seq_no, split_point):
+
+    # Request JSON.
+    request_body = {'data': left_output, 'frame_seq_no': frame_seq_no, 'split_point': split_point}
+
+    return request_body
+
+# Send HTTP request to server.
+def send_request(request_body, endpoint):
+
+    http_client = httpclient.HTTPClient(defaults=dict(connect_timeout = 10000000.0 ,request_timeout=100000000000.0))
+
+    body = pickle.dumps(request_body)
+
+    response =  http_client.fetch(url + endpoint,  method = 'POST', headers = None, body = body)
+
+    return response
+
+def handle_response(response):
+        
+    # Process the response here
+        response_data = pickle.loads(response.body)
+        result = response_data['result']
+        frame_seq_no = response_data['frame_seq_no']
+
+        Logger.log(f'Processed frame # {frame_seq_no}')
+        print(result.shape)
+
+def main_runner():
+
+    for split_point in range(1, 22):
+
+        print('SPLIT: ' + str(split_point))
+
+        left_model = get_left_model(split_point)
+
+        # Request JSON.
+        request_json = get_request_body(None, 0, split_point)
+
+        response = send_request(request_json, 'split_point')        
+
+        #Initialize total time taken for split_point
+        total_time_for_split_point = 0
+
+        frame_seq_no = 1
+    
+        global start_time
+        global frame_count
+        global left_output_size
+        
+        # This is the start of the video processing. Initialize the start time.
+        start_time = datetime.datetime.now()
+    
+        # Read the input from the file.
+        cam = cv2.VideoCapture('hdvideo.mp4')
+    
+    
+        while frame_seq_no < frames_to_process + 1:
+    
+            # Reading next frame from the input.       
+            ret, img_rbg = cam.read()   
+    
+            # If the frame exists
+            if ret: 
+                
+                Logger.log(f'[Inside main_runner] Frame # {frame_seq_no}: Send for left processing.')
+    
+                # Send the frame for left processing.
+                out_left = producer_video_left(img_rbg, left_model)
+
+                # Request JSON.
+                request_json = get_request_body(out_left, frame_seq_no, split_point)
+
+                # Send request to server for processing.
+                response = send_request(request_json, 'model')
+
+                handle_response(response)
+                    
+            # Increment frame count after left processing.    
+            frame_seq_no += 1
+    
+        # This is the end of the left processing. Set the end time of left video processing.
+        end_time = datetime.datetime.now()
+        
+        time = (end_time - start_time).total_seconds()
+        single_frame_time = time/frames_to_process
+
+        write_to_csv('vt.csv', metrics_headers, [split_point, flops, time, single_frame_time, left_output_size])
+        Logger.log(f'TOTAL TIME FOR PROCESSING:: {time} sec')
+        Logger.log(f'TIME TAKEN FOR SINGLE FRAME:: {single_frame_time} sec')
+
+        cam.release()
+        cv2.destroyAllWindows()
+
+
+with tf.device(device):
+
+    # Driver code.
+    if __name__ == '__main__':
+        main_runner()
---
+++ vt/vtServerSync.py
@@ -0,0 +1,96 @@
+import pickle
+import tensorflow as tf
+import tornado.ioloop
+
+from tensorflow import keras
+from vit_keras import vit
+from vtutils import Config, Logger
+
+
+# Read the configurations from the config file.
+config = Config.get_config()
+
+device = config['server_device']
+
+split_point = None
+
+with tf.device(device):
+    model = vit.build_model(image_size=224, patch_size=16, classes=1000, num_layers=19,
+                        hidden_size=768, num_heads=12, name= 'vit_custom', mlp_dim=3072,
+                        activation='softmax', include_top=True,
+                        representation_size=None)
+
+    #right_model = keras.Model(inputs=next_layer.input, outputs=model.output)
+    right_model = None
+
+
+class ModelHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Model done")
+    async def post(self):
+        #data = json.loads(self.request.body)
+            data =  pickle.loads(self.request.body)
+            json_dump_return_data = model_right(data)
+            self.write(json_dump_return_data)
+
+
+class SplitPointHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Model done")
+    async def post(self):
+        #data = json.loads(self.request.body)
+            data =  pickle.loads(self.request.body)
+            json_dump_return_data = set_model_right(data)
+            self.write(json_dump_return_data)
+
+def set_model_right(data):
+     
+     global right_model
+     global split_point
+
+     split_point = data['split_point']
+
+     next_layer = model.layers[split_point + 1]
+
+     print(f'Starting from layer # {split_point + 1} in server. Layer name: {next_layer.name}')
+
+     right_model = keras.Model(inputs=next_layer.input, outputs=model.output)
+
+     return pickle.dumps('Right model is ready.')
+
+
+def model_right(data):
+   
+   with tf.device(device):
+       
+       left_model_output = data['data']
+
+        # Transformer layers start from layer 5 and the output of the layer is two tensors. But the next layer input is only first tensor.            
+       if split_point >= 5:
+            left_model_output = left_model_output[0]
+
+       frame_seq_no = data['frame_seq_no']
+
+       Logger.log(f'Executing right model for frame #: {frame_seq_no}')
+
+       right_model_output = right_model(left_model_output)
+
+       return_data = {'result':right_model_output, 'frame_seq_no':frame_seq_no}
+
+       json_dump_return_data = pickle.dumps(return_data)
+
+       return json_dump_return_data       
+     
+
+def make_app():
+    return tornado.web.Application([
+        (r"/model", ModelHandler)
+        (r"/split_point", SplitPointHandler)
+    ])
+
+with tf.device(device):
+     if __name__ == "__main__":
+        app = make_app()
+        app.listen(8881)
+        print("Got a Call")
+        tornado.ioloop.IOLoop.current().start()
