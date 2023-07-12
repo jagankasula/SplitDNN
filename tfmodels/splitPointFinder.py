721914266dda634f07b2f49476d4b9e0a16f3cea
Jagannath Guptha Kasula <jagan@Jagannaths-MacBook-Air.local>
2023-07-12 11:38:37 -0500

    EfficeintNet tf

--- tfmodels/splitPointFinder.py
+++ tfmodels/splitPointFinder.py
@@ -6,16 +6,20 @@ import pandas as pd
 import io
 from modelUtils import get_flops
 
-model = tf.keras.applications.EfficientNetV2S(include_top=True,
+model = tf.keras.applications.EfficientNetV2B0(
+    include_top=True,
     weights="imagenet",
     input_tensor=None,
     input_shape=None,
     pooling=None,
     classes=1000,
     classifier_activation="softmax",
-    include_preprocessing=True,)
+    include_preprocessing=True,
+)
+    
 
 profile = model_profiler(model, 50)
+
 
 print(profile)
 
@@ -46,26 +50,28 @@ for split_point in range(1, len(layers) - 1):
     # print(split_layer.name)
     # print(next_layer.name)    
 
-    try: 
-        left_model = tf.keras.Model(inputs=model.input, outputs=split_layer.output)
+    left_model = tf.keras.Model(inputs=model.input, outputs=split_layer.output)
+
+    try:         
         right_model = tf.keras.Model(inputs=next_layer.input, outputs=model.output)
 
-        input = tf.random.uniform(shape=(None, 384, 384, 3))
+        input = tf.random.uniform(shape=(1, 224, 224, 3))
         left_output = left_model(input)
         right_output = right_model(left_output)
         print(f'Success at split point # {split_point}, Name: {split_layer.name}')
         model_profiler(left_model, 50)
         model_profiler(right_model, 50)
-        #print(model_profiler(left_model, 50))       
-        # print(input.shape)
-        # print(left_output.shape)
-        # print(right_output.shape)
+        print(model_profiler(left_model, 50))       
+        print(input.shape)
+        print(left_output.shape)
+        print(right_output.shape)
         candidate_layers.append(split_point)
     except ValueError as vrr:
         print(f'------- Failed at split point # {split_point}, Name: {split_layer.name}')
-        print(vrr)
+        #print(vrr)
     except Exception as e:
         print(f'------- EXCEPTION -- Failed at split point # {split_point}, Name: {split_layer.name}')
+        #print(e)
 
 print(candidate_layers)
 
