804dcca953f87de0f3a85be5d92664606a4a6485
Jagannath Guptha Kasula <jagan@jagannaths-air.lan>
2023-07-20 12:11:33 -0500

    latency measuere

--- testenv/clientTesting.py
+++ testenv/clientTesting.py
@@ -1,9 +1,18 @@
 import http.client
+import time
 
-conn = http.client.HTTPConnection('134.193.129.157', 8881)
-conn.request("GET", "/")
-response = conn.getresponse()
-print(response.status, response.reason)
-data = response.read()
-print(data.decode())
-conn.close()
+def measure_latency():
+    conn = http.client.HTTPConnection('KC-SCER-FCCT0M3', 8881)
+    start_time = time.time()
+    conn.request("GET", "/")
+    response = conn.getresponse()
+    latency = time.time() - start_time
+
+    print(f"Response Status: {response.status} {response.reason}")
+    print(f"Response Data: {response.read().decode()}")
+    print(f"Network Latency: {latency:.6f} seconds")
+
+    conn.close()
+
+if __name__ == "__main__":
+    measure_latency()
